55eb4ce48e3069396c891192366666735b2c1266
jrtate1 <143031865+jrtate1@users.noreply.github.com>
2026-05-28 23:00:23 -0400

    Add files via upload

---
+++ Midterm Project/Midterm-Pro-Mentor/js/main.js
@@ -0,0 +1,13 @@
+function loadComponent(id, path) {
+    fetch(path)
+      .then(res => res.text())
+      .then(html => {
+        document.getElementById(id).innerHTML = html;
+      });
+  }
+  
+  window.addEventListener("DOMContentLoaded", () => {
+    loadComponent("navbar", "components/navbar.html");
+    loadComponent("footer", "components/footer.html");
+  });
+  
